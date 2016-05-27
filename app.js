7dfb9fb8342f634a9fe36fa35eb86abc5b50ccd9
shudingbo <shudingbo@qq.com>
2016-05-27 10:52:38 +0800

    1. add sc and job parame to job callback function; 2. implement updateJob,runJob,stopJob function,now job can control it;

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 
 
 
-var sc = require("./lib/yz_schedule.js");
+var sc = require("sdb-schedule");
 var app = sc( './config.json');
+
+
+
+process.on('SIGINT', function () { //SIGINT这个信号是系统默认信号，代表信号中断，就是ctrl+c
+	console.log('Got SIGINT. exit.');
+	app.stop();
+});
+
 
 
 app.run();
