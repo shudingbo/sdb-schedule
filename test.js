2ecb0009c11a9d22a7ebde1df20fb53f5b6f8632
shudingbo <shudingbo@qq.com>
2016-06-04 16:42:01 +0800

    add redis config driver
    add redis config driver

--- test.js
+++ test.js
@@ -3,10 +3,23 @@
 
 
 var sc = require("./lib/yz_schedule.js"); // -- you code here require is 'sdb_schedule'
-var app = sc( { 
+
+/** File config Manager
+var app = sc({ 
 				'cfg_drv':'filedrv.js',
 				'cfg_opt':{
 					'cfgFile':"./config.json"
+				}
+			});
+*/
+
+/* redis config Manager*/
+var app = sc({ 
+				'cfg_drv':'redisdrv.js',
+				'cfg_opt':{
+					'host':"127.0.0.1",
+					'port':6379,
+					'keyPre':'sdb:schedule'
 				}
 			});
 
