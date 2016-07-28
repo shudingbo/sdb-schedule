546a0f35e15959314e9952d0ab796402854218b5
shudingbo <shudingbo@qq.com>
2016-07-28 16:18:32 +0800

    support sub job

--- sc/enableRoom.js
+++ sc/enableRoom.js
@@ -2,20 +2,30 @@
 var CornParser = require('cron-parser');
 
 module.exports = function(sc,job, runStep){
-	if( runStep === 0 ){
-		return init( sc,job );
-	}else if( runStep === 1 ){
-		return run( sc,job );
+	console.log( '------- in ',job['name']);
+
+	if( job['name'] !== 'subJob' ){
+		if( runStep === 0 ){
+			return init( sc,job );
+		}else if( runStep === 1 ){
+			return run( sc,job );
+		}else{
+			return stop( sc,job );
+		}
 	}else{
-		return stop( sc,job );
+		if( runStep === 1 ){
+			var parName = job.parent['name'];
+			sc.updateMsg( parName, '停止房间！' );
+		}
 	}
+
 };
 
 
 var g_cnt = 0;
 
 function init( sc, job ){
-	console.log( job['name'] + 'Next run Time' + CornParser.parseExpression(job['cron']).next() );
+	console.log( job['name'] + ': Next run Time: ' + CornParser.parseExpression(job['cron']).next() );
 }
 
 
@@ -25,18 +35,32 @@ function run( sc,job)
 	g_cnt++;
 	
 	console.log( job['name'] + "  " + g_cnt +" : " + job['cron'] );
-	if( g_cnt > 10 ){
+	if( g_cnt > 1000 ){
 		sc.stopJob( job['name'] );  // example stop this job
 	}
 	
-	if( g_cnt > 3 ){
-		sc.updateJob( job['name'], {
-			"cron":"*/2 * * * * *",
-			"fun":"./sc/enableRoom.js",
-			"switch":true
+	if( g_cnt == 2 ){
+		
+		//sc.updateJob( job['name'], {
+		//	"cron":"*/2 * * * * *",
+		//	"fun":"./sc/enableRoom.js",
+		//	"switch":true
+		//});
+		
+		sc.updateSubJob( 'subJob',{
+			"cron": (new Date()).valueOf()+15000,
+			"switch":true,
+			"parent":job['name']
 		});
+
+		sc.runJob( 'subJob' );
+
 	}
-	
+
+	if( g_cnt == 7 ){
+		sc.removeSubJob('subJob');
+	}
+
 	return 'Run OK';
 }
 
